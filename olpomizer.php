941028c2ca42fafb1eb0ab894de5d333ecd6d209
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-30 07:47:16 +0100

    Update olpomizer.php

--- olpomizer.php
+++ olpomizer.php
@@ -3,7 +3,7 @@
  * Plugin Name: OlpoMizer
  * Plugin URI: https://olpo.de
  * Description: Modulares WordPress Optimierungs-Plugin mit konfigurierbaren Features
- * Version: 1.4.8
+ * Version: 1.4.9
  * Author: Ole 
  * Author URI: https://olpo.de
  * Text Domain: olpomizer
